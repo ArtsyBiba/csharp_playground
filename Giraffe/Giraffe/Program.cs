a3c79afa5717098cc61220b6e70659647e83b12d
Artsy Biba <biba.artsiom@gmail.com>
2021-05-20 10:30:00 -0400

    testing out string and int variables

--- Giraffe/Giraffe/Program.cs
+++ Giraffe/Giraffe/Program.cs
@@ -6,10 +6,13 @@ namespace Giraffe
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("   /|");
-            Console.WriteLine("  / |");
-            Console.WriteLine(" /  |");
-            Console.WriteLine("/___|");
+            string characterName = "Alex";
+            int characterAge = 55;
+
+            Console.WriteLine("There once was a man named " + characterName);
+            Console.WriteLine("He was " + characterAge + " years old");
+            Console.WriteLine("He really liked the name " + characterName);
+            Console.WriteLine("But he didn't like being " + characterAge);
         }
     }
 }

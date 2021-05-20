d4beea4c7249c1f693ab0b9834602201a64fe1f0
Artsy Biba <biba.artsiom@gmail.com>
2021-05-20 10:42:59 -0400

    testing out double, char & bool variables

--- Giraffe/Giraffe/Program.cs
+++ Giraffe/Giraffe/Program.cs
@@ -7,12 +7,17 @@ namespace Giraffe
         static void Main(string[] args)
         {
             string characterName = "Alex";
+            char grade = 'A';
+
             int characterAge = 55;
+            double floatAge = 27.5;
+
+            bool isMale = true;
 
             Console.WriteLine("There once was a man named " + characterName);
             Console.WriteLine("He was " + characterAge + " years old");
-            Console.WriteLine("He really liked the name " + characterName);
-            Console.WriteLine("But he didn't like being " + characterAge);
+            Console.WriteLine("He really liked the name " + grade);
+            Console.WriteLine("But he didn't like being " + floatAge);
         }
     }
 }

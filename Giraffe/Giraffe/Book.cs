e81cf87cacd2984bb66017c420b5345bbc54abfd
Artsy Biba <biba.artsiom@gmail.com>
2021-05-22 15:00:07 -0400

    classes, object, constructors & object methods

---
+++ Giraffe/Giraffe/Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Giraffe
+{
+    class Book
+    {
+        public string title;
+        public string author;
+        public int pages;
+
+        public Book(string author, string title, int pages)
+        {
+            this.title = title;
+            this.author = author;
+            this.pages = pages;
+        }
+
+        public void PrintTitle()
+        {
+            Console.WriteLine(title);
+        }
+    }
+}
--- Giraffe/Giraffe/Program.cs
+++ Giraffe/Giraffe/Program.cs
@@ -6,37 +6,11 @@ namespace Giraffe
     {
         static void Main(string[] args)
         {
-            string secretWord = "giraffe";
-            string guess = "";
-            int count = 5;
-            bool isWinner = true;
+            Book book1 = new Book("JK Rowling", "Harry Potter", 431);
+            Book book2 = new Book("Tolkien", "Lord of The Rings", 645);
 
-            while (guess != secretWord) 
-            {
-                if (guess == "" && count > 0)
-                {
-                    Console.WriteLine("Guess an animal: ");
-                } else if (guess != "" && count > 0)
-                {
-                    Console.WriteLine($"Try again, you have {count} guesses left: ");
-                } else
-                {
-                    isWinner = false;
-                    break;
-                }
-                
-                guess = Console.ReadLine();
-                count--;
-            }
-            
-            if (isWinner)
-            {
-                Console.WriteLine("Cograts, you guessed it right!");
-            } else
-            {
-                Console.WriteLine("Sorry, you ran out of guesses!");
-            }
-            
+            book1.PrintTitle();
+            book2.PrintTitle();
         }
 
     }
